2827123d521d81478a6e13061c1855f1cb467ab9
Pratyush Vatsa <pratyushvatsa121000@gmail.com>
2025-05-10 18:53:21 +0530

    push

--- src/pages/PaymentSuccess.jsx
+++ src/pages/PaymentSuccess.jsx
@@ -1,12 +1,12 @@
 // src/pages/PaymentSuccess.jsx
 import { Container, Title, Text, Button } from '@mantine/core';
-import { CheckCircle } from 'tabler-icons-react';
+import { IconCircleCheck } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
 
 export default function PaymentSuccess() {
   return (
     <Container size="sm" py="xl">
-      <CheckCircle size={64} color="green" />
+      <IconCircleCheck size={64} color="green" />
       <Title order={1} mt="md">Payment Successful!</Title>
       <Text mt="sm" size="lg">
         Your mentorship session has been booked. Check your email for confirmation.
@@ -17,6 +17,7 @@ export default function PaymentSuccess() {
     </Container>
   );
 }
+
 
 
 
