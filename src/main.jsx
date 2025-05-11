d8b194c04a3bca346812617a22805aa82de5189b
Pratyush Vatsa <pratyushvatsa121000@gmail.com>
2025-05-11 17:52:21 +0530

    corrected

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { MantineProvider } from '@mantine/core';
 
-// Entry point for React app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
